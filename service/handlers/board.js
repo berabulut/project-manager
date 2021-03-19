d5c8827b44c1eb2836c200805a6f486fb65a994e
Hüseyin Bera Bulut <berabulut@gmail.com>
2021-03-19 19:23:12 +0300

    update board properties service

--- service/handlers/board.js
+++ service/handlers/board.js
@@ -3,6 +3,7 @@ const {
   inviteUser,
   returnBoardRelatedUsers,
   returnUserRelatedBoards,
+  updateBoardProperty,
 } = require("../src/boards");
 
 module.exports.create = async (event) => {
@@ -58,6 +59,60 @@ module.exports.create = async (event) => {
         body: JSON.stringify({
           statusCode: 400,
           message: "Title is undefined",
+        }),
+      };
+      resolve(response);
+    }
+  });
+  return promise;
+};
+
+module.exports.update = (event) => {
+  const promise = new Promise((resolve) => {
+    const { boardId, property, data } = JSON.parse(event.body);
+    if (boardId && property && data) {
+      updateBoardProperty(boardId, property, data)
+        .then((data) => {
+          const response = {
+            statusCode: 200,
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify({
+              statusCode: 200,
+              data: data,
+            }),
+          };
+          resolve(response);
+        })
+        .catch((err) => {
+          const response = {
+            statusCode: 500,
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify({
+              statusCode: 500,
+              error: err,
+            }),
+          };
+          resolve(response);
+        });
+    } else {
+      const response = {
+        statusCode: 400,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify({
+          statusCode: 400,
+          message: "Missing body element",
         }),
       };
       resolve(response);
