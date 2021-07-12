08c322f03708e5a683ca07a07d86ea81e3b407df
Hüseyin Bera Bulut <45367283+berabulut@users.noreply.github.com>
2021-07-12 21:25:16 +0300

    Delete index.js

--- service/index.js
+++
@@ -1,9 +0,0 @@
-const { returnImages } = require("./src/images");
-const { createUniqueId } = require("./src/boards");
-
-const main = async () => {
-  console.log(await returnImages());
-  console.log(await createUniqueId());
-};
-
-main()
